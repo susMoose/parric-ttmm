d332b0bd6945c58c362f108b05cd2332eab070fc
Louis Narmour <lnarmour@rams.colostate.edu>
2018-07-15 01:56:12 +0000

    created new logic

--- ttmm/mylib.c
+++ ttmm/mylib.c
@@ -6,7 +6,7 @@ int multiply(int, int);
 int main(int argc, char** argv) {
 	int x = atoi(argv[1]);
 	int y = atoi(argv[2]);
-	printf("%d * %d = %d\n", x, y, multiply(x,y));
+	printf("%d * %d = %d\n (ttmm-0)", x, y, multiply(x,y));
 	return 0;
 }
 
