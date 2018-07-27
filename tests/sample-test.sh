b8b3a5ba235599304a6e418b66617a8d669bb68d
Louis Narmour <lnarmour@rams.colostate.edu>
2018-07-27 10:25:39 -0600

    changed sample-test.sh, its basically a template

--- tests/sample-test.sh
+++ tests/sample-test.sh
@@ -1,1 +1,10 @@
-./ttmm/mylib 13 5
+# This doesn't actually do anything
+# Assuming that the TMM *.c code is in the ttmm/alphaz_stuff/out directory, we would want this script to do something like the following:
+# - execute make in the ttmm/alphaz_stuff/out directory to try to compile the code. If it doesn't compile, then report failed and quit
+# - after it compiles, actually do a few matrix multiplications, and write the output to some file
+# - look at the answer in that file and 'consult the oracle', to figure out if the answer matches what we expect.  If it doesn't then report failure and quit
+#
+# we could either put all of this into one big test script OR individual scripts and define individual steps in the jenkins file
+
+echo 'I did nothing'
+
