461f47103c9b07bcd8cd4ff7bb6c50662cd4470a
Louis Narmour <lnarmour@rams.colostate.edu>
2018-07-29 17:29:03 +0000

    added jenkins slave elcipse loc to cmdlinecompile

--- scripts/slave.sh
+++ scripts/slave.sh
@@ -12,13 +12,16 @@ docker run \
   --network jenkins-nw \
   --name slave \
   -d \
-  lnarmour/parric:jenkins-slave \
+  lnarmour/parric:jenkins-slave-eclipse-1.0 \
   -url http://jenkins:8080 \
   -workDir "/home/jenkins/slave" \
   $secret \
   slave
 
-sleep 3
+sleep 10
+
+docker exec -d slave Xvfb :1 -screen 0 1024x768x24
+docker exec -d slave /home/jenkins/eclipse/eclimd -b
 
 if [[ -n $(docker ps -f name=slave -q) ]]; then
    printf "SUCCESS : Created 'slave' successfully.\n"
