cac41357023c55af769a22a3657870b65a0f243c
Louis Narmour <lnarmour@rams.colostate.edu>
2018-08-04 11:16:02 -0600

    copy/pasted from jg

--- results/3dscatter.py
+++ results/3dscatter.py
@@ -8,16 +8,31 @@ if __name__ == "__main__":
         line=line.split(" ")
         print(line)
         point=( float(line[2][1:-1]), float(line[3][:-1]), float(line[4][:-1]))
-        point=(point[0], point[1], point[2])
-        points.append(point)
+        gflops = float(line[-2])
+        points.append((gflops, point))
+   
+        
     
+    points.sort()
+    series_boundary=0
+    for i in range(len(points)):
+        if points[i][0] > 12:
+            series_boundary=i
+            break
+    
+    
+    points=[p[1] for p in points]
     fig=plot.figure()
     graph=fig.add_subplot(111, projection="3d")
-    graph.scatter([p[0] for p in points], [p[1] for p in points], [p[2] for p in points] )
+    graph.scatter([p[0] for p in points[:series_boundary]], [p[1] for p in points[:series_boundary]], [p[2] for p in points[:series_boundary]], label="<12" )
+    
+    graph.scatter([p[0] for p in points[series_boundary:]], [p[1] for p in points[series_boundary:]], [p[2] for p in points[series_boundary:]], label=">12" )
+    
     graph.set_xlabel("ts1")
     graph.set_ylabel("ts2")
     graph.set_zlabel("ts3")
     graph.set_xlim3d(0,1000)
     graph.set_ylim3d(0,1000)
     graph.set_zlim3d(0,1000)
-plot.show()
+    graph.legend()
+    plot.show()
