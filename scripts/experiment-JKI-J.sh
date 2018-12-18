747882439edd4d1b86d03893502f213f8d9229e8
Louis Narmour <lnarmour@rams.colostate.edu>
2018-12-18 07:31:29 -0700

    added other experiment scripts

---
+++ scripts/experiment-JKI-J.sh
@@ -0,0 +1,4 @@
+python3 commander.py \
+-f experiment-config \
+--path-prefix ./workspace/parric-ttmm/parallelTests/jki/out/firstLoop \
+| tee -a ~/workspace/parric-ttmm/results/experiment-JKI-J.log
---
+++ scripts/experiment-JKI-K.sh
@@ -0,0 +1,4 @@
+python3 commander.py \
+-f experiment-config \
+--path-prefix ./workspace/parric-ttmm/parallelTests/jki/out/secndLoop \
+| tee -a ~/workspace/parric-ttmm/results/experiment-JKI-K.log
